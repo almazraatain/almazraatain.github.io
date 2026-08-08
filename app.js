d5143bf6deaa7ec2ec464ad5726508aead75534e
AlMazraatain <azozozo91@gmail.com>
2026-08-08 03:38:02 +0300

    ربط التطبيق بخادم Apps Script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 'use strict';
 
 /* ═══════════ الإعدادات ═══════════ */
-var API_DEFAULT = 'PASTE_EXEC_URL_HERE';
+var API_DEFAULT = 'https://script.google.com/macros/s/AKfycbz2Lhur_UImYicb6r64R4JL_0zS57Ok9Sgf1Ed1Orihog7_ey_nJe3bLTq7yitH_caPiA/exec';
 var API = localStorage.getItem('mzr_api') || API_DEFAULT;
 var KEY = 'mzr-key-2026-almazraatain';
 
